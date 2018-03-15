82ace8948a5b8965f979c6d56e0845a7569469f5
jeroentvb <jeroen.van.berkum@hva.nl>
2018-03-15 21:47:33 +0100

    Finished index, welcome, chats and login for the prototype

--- js/script.js
+++ js/script.js
@@ -8,14 +8,24 @@ function showMenu() {
   menu.classList.add('showMenu');
   menu.classList.remove('hideMenu');
 }
-
 function hideMenu() {
   menu.classList.remove('showMenu');
   menu.classList.add('hideMenu');
 }
 
-
-
 menuBtn.addEventListener('click', showMenu);
-
 closeMenu.addEventListener('click', hideMenu);
+
+
+if (document.getElementsByClassName('likeDislikeButtons')[0] !== undefined) {
+  var dislikeBtn = document.querySelector('.dislike');
+  var likeBtn = document.querySelector('.like');
+  var firstArticle = document.querySelector('#firstArticle');
+
+  dislikeBtn.addEventListener('click', function() {
+    firstArticle.innerHTML = '';
+  });
+  likeBtn.addEventListener('click', function() {
+    window.alert('It is a match!');
+  });
+}
