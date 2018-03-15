d8483c3a7712d7b39c437028d482206865a319ae
jeroentvb <jeroen.van.berkum@hva.nl>
2018-03-15 18:18:21 +0100

    added hamburger menu, added basic HTML to every page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+var menuBtn = document.querySelector('#menuBtn');
+var menu = document.querySelector('#hamburgerMenu');
+var closeMenu = document.querySelector('#closeMenu');
+
+function showMenu() {
+  menu.classList.add('showMenu');
+  menu.classList.remove('hideMenu');
+}
+
+function hideMenu() {
+  menu.classList.remove('showMenu');
+  menu.classList.add('hideMenu');
+}
+
+
+
+menuBtn.addEventListener('click', showMenu);
+
+closeMenu.addEventListener('click', hideMenu);
