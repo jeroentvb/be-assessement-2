38ed47ac4d0b359c65a8abe394a34044f6674050
jeroentvb <jeroen.van.berkum@hva.nl>
2018-03-16 11:04:30 +0100

    Log in/sign up pages done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-var menuBtn = document.querySelector('#menuBtn');
-var menu = document.querySelector('#hamburgerMenu');
-var closeMenu = document.querySelector('#closeMenu');
+// Mobile menu
+if (document.getElementById('hamburgerMenu') !== null) {
+  var menuBtn = document.querySelector('#menuBtn');
+  var menu = document.querySelector('#hamburgerMenu');
+  var closeMenu = document.querySelector('#closeMenu');
 
-function showMenu() {
-  menu.classList.add('showMenu');
-  menu.classList.remove('hideMenu');
+  var showMenu = function() {
+    menu.classList.add('showMenu');
+    menu.classList.remove('hideMenu');
+  };
+  var hideMenu = function() {
+    menu.classList.remove('showMenu');
+    menu.classList.add('hideMenu');
+  };
+
+  menuBtn.addEventListener('click', showMenu);
+  closeMenu.addEventListener('click', hideMenu);
 }
-function hideMenu() {
-  menu.classList.remove('showMenu');
-  menu.classList.add('hideMenu');
-}
 
-menuBtn.addEventListener('click', showMenu);
-closeMenu.addEventListener('click', hideMenu);
-
-
+// Like/dislike buttons
 if (document.getElementsByClassName('likeDislikeButtons')[0] !== undefined) {
   var dislikeBtn = document.querySelector('.dislike');
   var likeBtn = document.querySelector('.like');
@@ -27,5 +30,15 @@ if (document.getElementsByClassName('likeDislikeButtons')[0] !== undefined) {
   });
   likeBtn.addEventListener('click', function() {
     window.alert('It is a match!');
+  });
+}
+
+// Back button
+if (document.getElementById('backBtn') !== null) {
+  var backBtn = document.querySelector('#backBtn');
+
+  backBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.history.back();
   });
 }
