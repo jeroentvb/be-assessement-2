19c555069d65526616a6c40bbd1d20ca9dff1032
jeroentvb <jeroen.van.berkum@hva.nl>
2018-04-09 14:07:08 +0200

    Added database

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is a repo for my dating website for project tech.
 It's built using the express framework for nodejs.
 
 Run `npm install` before running the server
+
+The server will likely not run since you are missing the `.env` file and a `SQL database`.
--- index.js
+++ index.js
@@ -3,9 +3,29 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var multer = require('multer')
 var mysql = require('mysql')
+var bcrypt = require('bcrypt')
 var chalk = require('chalk')
 
 require('dotenv').config()
+
+// bycript
+const saltRounds = 10
+
+// create mysql connection
+var db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+// connect to db
+db.connect(function(err) {
+  if(err){
+    throw err;
+  } else {
+    console.log(chalk.green('[MySql] connection established..'))
+  }
+})
 
 var data = [
   {
@@ -41,6 +61,9 @@ var data = [
 
 // Add HTTP requests
 module.exports = express()
+  // .get('/createdb', createDb)
+  // .get('/addusr', addUsr)
+
   .set('view engine', 'ejs')
   .set('views', 'view')
   .use(express.static('static'))
@@ -51,45 +74,99 @@ module.exports = express()
 
   .get('/profile', render)
   .get('/chatlist', render)
-  // .get('/error', render)
   .get(/html/, render)
-  .post('/', addUser)
+  .post('/', register)
   .get('/register', render)
+  .get('/login', render)
   // .use('/', render)
 
   .use(notFound)
-  .listen(3000, () => console.log(chalk.green('server running on port 3000...')))
+  .listen(3000, () => console.log(chalk.green('[Server] listening on port 3000...')))
 
+// // create db
+// function createDb(req, res) {
+//   var sql = 'CREATE DATABASE datingsite'
+//   db.query(sql, function(err, result) {
+//     if(err){
+//       throw err
+//     } else {
+//       console.log(chalk.yellow(result))
+//       res.send('Database created')
+//     }
+//   })
+// }
+
+// // create established
+// function addUsr(req, res) {
+//   var sql = 'CREATE TABLE users(id int NOT NULL AUTO_INCREMENT, email VARCHAR(255), password VARCHAR(255), name VARCHAR(255), tagline VARCHAR(255), PRIMARY KEY (id))'
+//   db.query(sql, function(err, result) {
+//     if(err) {
+//       throw err
+//     } else {
+//       console.log(chalk.yellow(result))
+//       res.send('Table created')
+//     }
+//   })
+// }
+
+// render index
 function index(req, res) {
   // log req.path
-  console.log(chalk.red('Requested path was ' + req.path))
+  console.log(chalk.red('[Server] Requested path was ' + req.path))
   // render index.ejs
   res.render('index', {data: data})
 }
 
+// get path and render page
 function render(req, res) {
   // get the url and put it in a var
   var reqPath = req.path.replace('/', '').replace('.html', '')
   // try to render the requested url
-  console.log(chalk.red('The req url was: ' + reqPath))
+  console.log(chalk.red('[Server] The req url was: ' + reqPath))
   // render page
   res.render(reqPath)
 }
 
-// function form(req, res) {
-//
-//   res.render('register')
-// }
-
-function addUser(req, res) {
-  data.push({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
-  res.redirect('/')
+// register
+function register(req, res) {
+  // get the contents of the form and put them in variables
+  var name = req.body.name
+  var email = req.body.email
+  var passwd = req.body.password
+  // define min and max passwd length
+  var passwdLength = {
+    min: 6,
+    max: 100
+  }
+  // check if everything is filled in
+  if (!name || !email || !passwd) {
+    res.status(400).send('Name, email or password are missing!')
+    return
+  }
+  // check if passwd is long enough but not too long
+  if (passwd.length < passwdLength.min || passwd.length > passwdLength.max) {
+    res.status(400).send(`Your password must be between ${passwdLength.min} and ${passwdLength.max} characters.`)
+    return
+  }
+  // hash the passwd and send the form data to the database
+  bcrypt.hash(passwd, saltRounds, function(err, hash) {
+  db.query('INSERT INTO users SET ?', {
+    name: name,
+    email: email,
+    password: hash
+  }, done)
+})
+  // check if an error ocurred, else: redirect to /
+  function done(err, data) {
+    if(err){
+      next(err)
+    } else {
+      res.redirect('/')
+    }
+  }
 }
 
+// handle 'couldn't get ... requests'
 function notFound(req, res) {
   res.status(404).render('error', {error: '404 Not found. We could not find this page :('})
 }
