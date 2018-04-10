40e26a2777c8cbebf8cca943b59cfb870e542859
jeroentvb <jeroen.van.berkum@hva.nl>
2018-04-10 14:10:39 +0200

    Added logging in & started improving templates

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 This is a repo for my dating website for project tech.
 It's built using the express framework for nodejs.
 
-Run `npm install` before running the server
-
-The server will likely not run since you are missing the `.env` file and a `SQL database`.
+## How to use
+1. Run `npm install` & `npm build`.
+2. Create a `.env` file containing the following:
+```
+DB_HOST=localhost
+DB_USER=root
+DB_NAME=datingsite
+DB_PASSWORD= YOUR DATABASE PASSWORD
+SESSION_SECRET= YOUR 'SECRET' express sessions
+```
+3. Uncomment the `createDb` & `addUsr` functions, as well as `.get('/createdb', createDb)` & `.get('/addusr', addUsr)`
+4. Host a mysql server (I used xampp on windows)
+5. Run the server by navigating to the folder in the command line and either running `nodemon` or `npm start` (I reccommend nodemon as it restarts the server after saving a change in index.js)
+5. Open your browser and go to `localhost:3000/createdb`. If everything is configured correctly you will see __'Database created'__ in the console.
+6. Go to `localhost:300/addusr`. If everything is configured correctly you will see __'Table created'__ in the console. The database has now been set up.
+7. Comment (or remove) the`createDb` & `addUsr` functions as well as `.get('/createdb', createDb)` & `.get('/addusr', addUsr)`.
+8. You now need to populate the database. You can do this using `localhost:3000/register` or do it manually.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require everything needed
 var express = require('express')
+var session = require('express-session')
 var bodyParser = require('body-parser')
 var multer = require('multer')
 var mysql = require('mysql')
@@ -7,6 +8,9 @@ var bcrypt = require('bcrypt')
 var chalk = require('chalk')
 
 require('dotenv').config()
+
+// multer
+var upload = multer({dest: 'static/upload/'})
 
 // bycript
 const saltRounds = 10
@@ -68,17 +72,22 @@ module.exports = express()
   .set('views', 'view')
   .use(express.static('static'))
   .use(bodyParser.urlencoded({extended: true}))
+  .use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET
+  }))
   .get('/', index)
   .get('/index', index)
   .get('index.html', index)
-
-  .get('/profile', render)
+  .get('/profile' || 'profile.html', profile)
   .get('/chatlist', render)
   .get(/html/, render)
   .post('/', register)
   .get('/register', render)
   .get('/login', render)
-  // .use('/', render)
+  .post('/log-in', login)
+  .get('/log-out', logout)
 
   .use(notFound)
   .listen(3000, () => console.log(chalk.green('[Server] listening on port 3000...')))
@@ -96,7 +105,7 @@ module.exports = express()
 //   })
 // }
 
-// // create established
+// // create create users table
 // function addUsr(req, res) {
 //   var sql = 'CREATE TABLE users(id int NOT NULL AUTO_INCREMENT, email VARCHAR(255), password VARCHAR(255), name VARCHAR(255), tagline VARCHAR(255), PRIMARY KEY (id))'
 //   db.query(sql, function(err, result) {
@@ -112,9 +121,55 @@ module.exports = express()
 // render index
 function index(req, res) {
   // log req.path
-  console.log(chalk.red('[Server] Requested path was ' + req.path))
-  // render index.ejs
-  res.render('index', {data: data})
+  console.log(chalk.yellow('[Server] Requested path was ' + req.path))
+  // Get random selection of users from db
+  db.query('SELECT name, tagline FROM users ORDER BY RAND() LIMIT 3', done)
+  function done(err, results) {
+    // JSON.stringify(results, null, 4) <-- got this from https://stackoverflow.com/questions/1625208/print-content-of-javascript-object?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    console.log(chalk.red('These are the query results: \n' + JSON.stringify(results, null, 4)))
+    // render index.ejs
+    res.render('index', {
+      users: results
+    })
+  }
+}
+
+function profile(req, res, next) {
+  console.log(chalk.yellow('[Server] Requested path was ' + req.path))
+  // console.log(chalk.red(`req.session.user is ${req.session.user}`))
+  // console.log(chalk.red(`req.session.email is ${req.session.email}`))
+  if (req.session.user == undefined) {
+    res.status(401).render('needlogin')
+  } else {
+    var currentUser = req.session.user.name
+    // Log current session user's name
+    // console.log(chalk.blue(`the current user is ${currentUser}`))
+
+    db.query('SELECT tagline FROM users WHERE name = ?', currentUser, done)
+
+    function done(err, results) {
+      // Log the data got from the db
+      console.log(chalk.red('These are the query results: \n' + JSON.stringify(results, null, 4)))
+      // log the tagline
+      // console.log(chalk.blue(results[0].tagline))
+      if (results[0].tagline == null) {
+        // If there is no tagline
+        res.render('profile', {
+          user: req.session.user,
+          tagline: 'You have no tagline set'
+        })
+      } else if(err) {
+        // if error
+        next(err)
+      } else {
+        // if everything is found
+        res.render('profile', {
+          user: req.session.user,
+          tagline: results[0].tagline
+        })
+      }
+    }
+  }
 }
 
 // get path and render page
@@ -122,7 +177,7 @@ function render(req, res) {
   // get the url and put it in a var
   var reqPath = req.path.replace('/', '').replace('.html', '')
   // try to render the requested url
-  console.log(chalk.red('[Server] The req url was: ' + reqPath))
+  console.log(chalk.yellow('[Server] The req url was: ' + reqPath))
   // render page
   res.render(reqPath)
 }
@@ -161,9 +216,68 @@ function register(req, res) {
     if(err){
       next(err)
     } else {
+      req.session.user = {email: user.email}
       res.redirect('/')
     }
   }
+}
+
+// Log in
+function login(req, res, next) {
+  // Get form data
+  var email = req.body.email
+  var passwd = req.body.password
+
+  // If there is form data missing give an error
+  if (!email || !passwd) {
+    res.status(400).send('Email or password are missing!')
+    return
+  }
+  // get email from form and look if it exists in the database
+  db.query('SELECT * FROM users WHERE email = ?', email, done)
+
+  function done(err, data) {
+    var user = data && data[0]
+    // log the user object
+    // console.log(chalk.red(JSON.stringify(user, null, 4)))
+    // log the user's name
+    // console.log(chalk.blue(user.name))
+    // log the user's email
+    // console.log(chalk.blue(user.email))
+
+    if (err) {
+      next(err)
+    } else if (user) {
+      // if the email is found, get the password and check if it matches
+      bcrypt.compare(passwd, user.password).then(onverify, next)
+    } else {
+      // send error if email doesn't exist in db
+      res.status(401).send('email does not exist!')
+    }
+
+    function onverify(match) {
+      if (match) {
+        // set session cookie and redirect
+        req.session.user = {name: user.name}
+        // log the session.user object
+        // console.log(chalk.red(JSON.stringify(req.session.user, null, 4)))
+        res.redirect('/profile')
+      } else {
+        // error if the password is incorrect
+        res.status(401).send('Incorrect password')
+      }
+    }
+  }
+}
+
+function logout(req, res, next) {
+  req.session.destroy(function (err) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/')
+    }
+  })
 }
 
 // handle 'couldn't get ... requests'
